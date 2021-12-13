4e16eb4ede96c95bd87fe288b034d6c8fdef9ec3
unknown <akshayHVshinde04@gmail.com>
2021-12-13 16:37:01 +0530

    public static void main(String[] args) {

--- src/com/bl/Algorithm.java
+++ src/com/bl/Algorithm.java
@@ -1,31 +1,25 @@
 package com.bl;
 
 public class Algorithm {
-	public int[] insertionSort(int[] array) {
-		for (int i = 0; i < array.length; i++) {
-			int num1 = array[i];
-			int temp = i - 1;
-			while (temp >= 0 && array[temp] > num1) {
-				array[temp + 1] = array[temp];
-				temp = temp - 1;
-			}
-			array[temp + 1] = num1;
-		}
-		return array;
-	}
-
-	public void displaySort(int[] array) {
-		for (int num2 : array) {
-			System.out.print(" " + num2);
-		}
-		System.out.println();
-	}
-
 	public static void main(String[] args) {
-		Algorithm sort = new Algorithm();
+		System.out.print("Prime Number between 0-1000 are : ");
+		for (int num = 0; num <= 1000; num++) {
+			if (isPrime(num)) {
+				System.out.print(num + " ");
+			}
+			System.out.print("");
+		}
+	}
 
-		int array[] = { 40, 30, 10, 70, 50, 20, 60 };
-		int[] sortedArray = sort.insertionSort(array);
-		sort.displaySort(sortedArray);
+	public static boolean isPrime(int num) {
+		if (num == 0 || num == 1) {
+			return false;
+		}
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				return false;
+			}
+		}
+		return true;
 	}
 }
