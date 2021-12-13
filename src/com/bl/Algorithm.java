5364dfff7ab937e69664680a005323f42b3eade5
unknown <akshayHVshinde04@gmail.com>
2021-12-13 17:07:19 +0530

    Anagram

--- src/com/bl/Algorithm.java
+++ src/com/bl/Algorithm.java
@@ -1,59 +1,30 @@
 package com.bl;
+
+import java.lang.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Algorithm {
 	public static void main(String[] args) {
 
-		System.out.println("Prime Numbers that are Palindrome As well As Anagram ");
-		for (int i = 2; i <= 1000; i++) {
-			if (isPrime(i) == true && isPalindrome(i)) {
-				boolean result = isAnagram(String.valueOf(i), String.valueOf(i));
-				if (result == true) {
-					System.out.println(" " + i);
-				}
-			}
-		}
-	}
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the first string");
+		String word1 = scan.next();
+		System.out.println("Enter the second string");
+		String word2 = scan.next();
 
-	public static boolean isPrime(int num) {
-		if (num == 0 || num == 1) {
-			return false;
-		}
-		for (int i = 2; i < num; i++) {
-			if (num % i == 0) {
-				return false;
-			}
-		}
-		return true;
-	}
+		char[] charArray1 = word1.toCharArray();
+		char[] charArray2 = word2.toCharArray();
 
-	public static boolean isPalindrome(int num) {
-		int r, sum = 0;
-		int temp = num;
-		while (num > 0) {
-			r = num % 10;
-			sum = (sum * 10) + r;
-			num = num / 10;
-		}
-		if (temp == sum) {
-			return true;
+		Arrays.sort(charArray1);
+		Arrays.sort(charArray2);
+
+		boolean result = Arrays.equals(charArray1, charArray2);
+
+		if (result == true) {
+			System.out.println("Strings are Anagram");
 		} else {
-			return false;
+			System.out.println("Strings are NOT Anagram");
 		}
-	}
-
-	public static boolean isAnagram(String str1, String str2) {
-		char[] arr1 = str1.toCharArray();
-		char[] arr2 = str2.toCharArray();
-		if (str1.length() != str2.length()) {
-			return false;
-		} else {
-			Arrays.sort(arr1);
-			Arrays.sort(arr2);
-			if (Arrays.equals(arr1, arr2)) {
-				return true;
-			}
-		}
-		return false;
 	}
 }
