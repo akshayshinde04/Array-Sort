bc835aa1cb2c7fe0525962badfd2eb5d3931b8e3
unknown <akshayHVshinde04@gmail.com>
2021-12-13 17:04:05 +0530

    PalindromeAnagram

--- src/com/bl/Algorithm.java
+++ src/com/bl/Algorithm.java
@@ -1,13 +1,17 @@
 package com.bl;
+import java.util.Arrays;
 
 public class Algorithm {
 	public static void main(String[] args) {
-		System.out.print("Prime Number between 0-1000 are : ");
-		for (int num = 0; num <= 1000; num++) {
-			if (isPrime(num)) {
-				System.out.print(num + " ");
+
+		System.out.println("Prime Numbers that are Palindrome As well As Anagram ");
+		for (int i = 2; i <= 1000; i++) {
+			if (isPrime(i) == true && isPalindrome(i)) {
+				boolean result = isAnagram(String.valueOf(i), String.valueOf(i));
+				if (result == true) {
+					System.out.println(" " + i);
+				}
 			}
-			System.out.print("");
 		}
 	}
 
@@ -21,5 +25,35 @@ public class Algorithm {
 			}
 		}
 		return true;
+	}
+
+	public static boolean isPalindrome(int num) {
+		int r, sum = 0;
+		int temp = num;
+		while (num > 0) {
+			r = num % 10;
+			sum = (sum * 10) + r;
+			num = num / 10;
+		}
+		if (temp == sum) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isAnagram(String str1, String str2) {
+		char[] arr1 = str1.toCharArray();
+		char[] arr2 = str2.toCharArray();
+		if (str1.length() != str2.length()) {
+			return false;
+		} else {
+			Arrays.sort(arr1);
+			Arrays.sort(arr2);
+			if (Arrays.equals(arr1, arr2)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
